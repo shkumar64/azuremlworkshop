7b222eaa62b2e4d8c3dc3b9f26d888d1e034ca2d
Ashish.Talati <Ashish.Talati@microsoft.com>
2020-05-20 18:11:37 +0000

    Updated score.py

--- score.py
+++ score.py
@@ -20,7 +20,7 @@ def init():
     global model
     # This name is model.id of model that we want to deploy deserialize the model file back
     # into a sklearn model
-    model_path = Model.get_model_path('IBM_attrition_model')
+    model_path = Model.get_model_path('attrition_model')
     model = joblib.load(model_path)
 
 
